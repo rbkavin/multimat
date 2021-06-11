67feb02eeb8d0d18fdb6c609a2d375eac7e593d4
kavin kumar <bkavin001@gmail.com>
2021-06-11 09:43:53 +0530

    Add files via upload

--- multimattap/scripts/script.js
+++ multimattap/scripts/script.js
@@ -3,18 +3,34 @@ const Scene = require('Scene');
 const Patch = require('Patches');
 const Material = require('Materials');
 const Touch = require('TouchGestures');
-var matname = Patch.getStringValue("matname");
-var objname = Patch.getStringValue("objname");
-var totmat = Patch.getScalarValue("totmat");
 
-var i = 1;
-Touch.onTap().subscribe(()=>{
-i++;
-if(i==(totmat.pinLastValue()+1))
-{
-i=1;
-}
-var mat = matname.pinLastValue()+''+i.toString();
-Diagnostics.log(mat);
-Scene.root.find(objname.pinLastValue()).material = Material.get(mat);
-});
+(async () => {
+
+    const [totMAt, objectName, matName] = await Promise.all([
+        Patch.outputs.getScalar("totmat"),
+        Patch.outputs.getString("objname"),
+        Patch.outputs.getString("matname"),
+
+
+    ])
+    const [object] = await Promise.all([
+        Scene.root.findFirst(objectName.pinLastValue())
+    ])
+    let mat = [];
+
+    for (let i = 0; i <= totMAt.pinLastValue(); i++) {
+        mat.push(Material.findFirst(matName.pinLastValue() + i))
+    }
+    const materials = await Promise.all(mat);
+
+
+    var i = 0;
+    Touch.onTap().subscribe(() => {
+        i++;
+        if (i == (totMAt.pinLastValue())) {
+            i = 0;
+        }
+        object.material = materials[i];
+    });
+
+})()
